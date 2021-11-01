7021fb743fe6e4b7ec5f04eb37fa08d743b94dbf
lss <ss_li@shenjumiaosuan.com>
2021-11-01 10:44:57 +0800

    初始化项目

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  semi: false,
-  trailingComma: "all",
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  endOfLine: "auto",
-  trailingComma: "none"
+    semi: false,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 2,
+    endOfLine: 'auto',
+    trailingComma: 'none'
 };
